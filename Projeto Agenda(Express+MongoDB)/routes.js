9a12dff79440f64b73ffe4d693ae79b6abb6b145
Daviniel <eng.daviniel@gmail.com>
2023-06-27 16:27:17 -0300

    modificando rota

--- Projeto Agenda(Express+MongoDB)/routes.js
+++ Projeto Agenda(Express+MongoDB)/routes.js
@@ -8,7 +8,7 @@ const loginController = require('./src/controllers/loginController');
 route.get('/', homeController.index);
 
 // Rotas de login
-route.get('/login/index', loginController.index);
+route.get('/login/', loginController.index);
 
 
 module.exports = route;
