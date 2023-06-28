bfe865f6ec4afedbfe67b652428e6e7e6eea6e7a
Daviniel <eng.daviniel@gmail.com>
2023-06-28 15:08:06 -0300

    adicionado o login e usuario

--- Projeto Agenda(Express+MongoDB)/routes.js
+++ Projeto Agenda(Express+MongoDB)/routes.js
@@ -9,6 +9,7 @@ route.get('/', homeController.index);
 
 // Rotas de login
 route.get('/login/', loginController.index);
+route.get('/login/register', loginController.register);
 
 
 module.exports = route;
