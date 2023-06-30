ca7c24f0e3bfa666a4ef1349bc8b69208f36719b
Daviniel <eng.daviniel@gmail.com>
2023-06-30 10:11:01 -0300

    criado e finalizado login e logout de usuario

--- Projeto Agenda(Express+MongoDB)/routes.js
+++ Projeto Agenda(Express+MongoDB)/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 
@@ -8,8 +9,10 @@ const loginController = require('./src/controllers/loginController');
 route.get('/', homeController.index);
 
 // Rotas de login
-route.get('/login/', loginController.index);
-route.get('/login/register', loginController.register);
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
 
 module.exports = route;
