a3bc3a01b8504fb0ff3b0ff6cb6a8204a0c8855e
Daviniel <eng.daviniel@gmail.com>
2023-07-18 17:32:46 -0300

    adicionado alguns styles globais

--- React/react-base/src/pages/Login/index.js
+++ React/react-base/src/pages/Login/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled'
+
 export default function Login() {
-  return <h1>Login</h1>
+  return (
+    <>
+      <Container>
+        <Title>
+          Login
+          <small>Oie</small>
+        </Title>
+        <Paragrafo>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores impedit vel neque ullam unde quasi expedita, ex consectetur nam sunt delectus aliquam perspiciatis corrupti sint, natus dolorum! Facilis, eius accusamus!</Paragrafo>
+        <a href="">Olie</a>
+      </Container>
+    </>
+  );
 }
---
+++ React/react-base/src/pages/Login/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  background: red;
+  small {
+    color: blue;
+  }
+`;
+
+export const Paragrafo = styled.p`
+`;
