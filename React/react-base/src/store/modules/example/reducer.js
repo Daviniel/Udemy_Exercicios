617c43428b0a102de7b4563da1596b409d27228f
Daviniel <eng.daviniel@gmail.com>
2023-08-01 17:26:40 -0300

    configurando o redux saga

--- React/react-base/src/store/modules/example/reducer.js
+++ React/react-base/src/store/modules/example/reducer.js
@@ -1,10 +1,24 @@
+import * as types from '../types';
+
 const initialState = {
   botãoClicado: false,
 };
 
 export default function (state = initialState, action) => {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCESS: {
+      const newState = { ...state };
+      newState.botaoClicado = !newState.botaoClicado;
+      return newState;
+    }
+
+    case types.BOTAO_CLICADO_REQUEST: {
+      const newState = { ...state };
+      newState.botaoClicado = !newState.botaoClicado;
+      return newState;
+    }
+
+    case types.BOTAO_CLICADO_FAULURE: {
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
