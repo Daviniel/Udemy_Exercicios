fba1bca3dff0495bf5c3f5383ff6144fa65dae0f
Daviniel <eng.daviniel@gmail.com>
2023-08-04 14:03:38 -0300

    corrigindo escritas e algumas regras do eslint

--- React/react-base/src/pages/Page404/index.js
+++ React/react-base/src/pages/Page404/index.js
@@ -5,7 +5,8 @@ import { Container } from '../../styles/GlobalStyles';
 export default function Page404() {
   return (
     <Container>
-      <h1>Essa página não existe</h1>;
+      <h1>Essa página não existe</h1>
+      ;
     </Container>
   );
 }
--- React/react-base/src/store/modules/reduxPersist.js
+++ React/react-base/src/store/modules/reduxPersist.js
@@ -1,14 +1,14 @@
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
 
-export default reducers => {
+export default (reducers) => {
   const persistedReducers = persistReducer(
     {
       key: 'REACT-BASE',
       storage,
       whitelist: ['example'],
     },
-    reducers
+    reducers,
   );
 
   return persistedReducers;
