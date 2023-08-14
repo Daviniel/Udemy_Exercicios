f9735afd44c67488767df6dc0ddf4c4f15010f51
Daviniel <eng.daviniel@gmail.com>
2023-08-14 19:00:42 -0300

    login finalizado

--- React/consumirapi/src/pages/Login/index.js
+++ React/consumirapi/src/pages/Login/index.js
@@ -5,10 +5,13 @@ import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
 import * as actions from '../../store/modules/auth/actions';
+import { get } from 'lodash';
 
 
 export default function Login() {
   const dispatch = useDispatch();
+
+  const prevPath = get(props, 'location.state.prevPaht', '/');
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -29,7 +32,7 @@ export default function Login() {
 
     if (formErrors) return;
 
-    dispatch(actions.loginRequest({ email, password }));
+    dispatch(actions.loginRequest({ email, password, prevPath }));
   };
 
   return (
