ddc77813c62b48b43f68e662f588eb41a674bc52
Daviniel <eng.daviniel@gmail.com>
2023-08-18 12:17:30 -0300

    edição de dados do usuario finalizada

--- React/consumirapi/src/store/modules/auth/sagas.js
+++ React/consumirapi/src/store/modules/auth/sagas.js
@@ -29,13 +29,51 @@ function persistRehydrate({ payload }) {
   axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
+// eslint-disable-next-line consistent-return
 function registerRequest({ payload }) {
   const { id, nome, email, password } = payload;
-  password: password ||
+
+  try {
+    if (id) {
+      yield call(axios.put, '/users', {
+        email,
+        nome,
+        password: password || undefined,
+      });
+      toast.success('Dados alterados com sucesso!');
+      yield put(actions.registerUpdatedSuccess({ nome, email, password }));
+      history.push('/çpgin');
+    } else {
+      yield call(axios.post, '/users', {
+        email,
+        nome,
+        password,
+      });
+      toast.success('Conta criada com sucesso!');
+      yield put(actions.registerCreatedSuccess({ nome, email, password }));
+    }
+  } catch (e) {
+    const errors = get(e, 'response.data.error', []);
+    const status = get(e, 'response.status', 0);
+
+    if (status === 401) {
+      toast.error('Você precisa fazer login novamente.');
+      yield put(action.loginFailure());
+      return history.push('/login');
+    }
+
+    if (errors, length > 0) {
+      errors.map(error => toast.error(error));
+    } else {
+      toast.error('Erro desconhecido');
+    }
+
+    yield put(actions.registerFailure());
+  }
 }
 
 export default all([
-  takeLatest(types.LOGIN_REQUEST, exampleRequest),
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
   takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
   takeLatest(types.REGISTER_REQUEST, registerRequest),
 ]);
