1dc1c2eb8a696865022b7e26b09a11098c3dddd4
Daviniel <eng.daviniel@gmail.com>
2023-08-21 14:24:32 -0300

    metodo de excluir alunos finalizado

--- React/consumirapi/src/pages/Alunos/index.js
+++ React/consumirapi/src/pages/Alunos/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { get } from 'lodash';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+import { FaUserCircle, FaEdit, FaWindowClose, FaExclamation } from 'react-icons/fa';
 import { Container } from '../../styles/GlobalStyles';
 import axios from '../../services/axios';
 import { AlunoContainer, ProfilePicture } from './styled';
+import { toast } from 'react-toastify';
 
 
 export default function Alunos() {
@@ -19,13 +20,43 @@ export default function Alunos() {
     getData();
   }, []);
 
+  const handleDeleteAsk = e => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    e.currentTarget.remove();
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+
+    try {
+      await axios.delete(`/alunos/${id}`);
+      const novosAlunos = [...alunos];
+      novosAlunos.splice(index, 1);
+      setAlunos(novosAlunos);
+    } catch (err) {
+      const status = get(err, 'response.status', 0);
+
+      if (status === 401) {
+        toast.error('Você precisa fazer login');
+      } else {
+        toast.error('Ocorreu um erro ao excluir aluno');
+      }
+
+    }
+  };
+
   return (
     <Container>
       <h1>Alunos</h1>
 
       <AlunoContainer>
-        {alunos.map(aluno => (
+
+        {alunos.map((aluno, index) => (
+
           <div key={String(aluno.id)}>
+
             <ProfilePicture>
               {get(aluno, 'Fotos[0].url', false) ? (
                 <img src='' />  /* aqui será adicionado as imagens dos alunos */
@@ -40,9 +71,17 @@ export default function Alunos() {
             <Link to={`/aluno/${aluno.id}/edit`}>
               <FaEdit size={16} />
             </Link>
+
             <Link to={`/aluno/${aluno.id}/delete`}>
               <FaWindowClose size={16} />
             </Link>
+
+            <FaExclamation
+              size={16}
+              display="none"
+              cursor="pointer"
+              onClick={e => handleDelete(e, aluno.id,)}
+            />
           </div>
         ))}
       </AlunoContainer>
