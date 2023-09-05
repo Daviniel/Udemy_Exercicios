04a424e876ff07d26ce6c36804553d61f7576c8a
Daviniel <eng.daviniel@gmail.com>
2023-09-05 12:09:20 -0300

    styled da pagina finalizada

--- React/consumirapi/src/pages/Alunos/styled.js
+++ React/consumirapi/src/pages/Alunos/styled.js
@@ -1,30 +1,49 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import * as colors from '../../config/colors';
 
-export const AlunoContainer = styled.div`
-  margin-top: 20px;
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
 
-  div {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5px 0;
-  }
-
-  div + div {
-    border-top: 1px solid #eee;
+  input {
+    height: 40px;
+    margin-bottom: 20px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 0 10px;
   }
 `;
 
 export const ProfilePicture = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 0 20px;
+  position: relative;
+  margin-top: 20px;
+
   img {
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    position: absolute;
+    bottom: 0;
+    color: #fff;
+    background: ${colors.primaryColor};
     width: 36px;
     height: 36px;
     border-radius: 50%;
   }
 `;
 
-export const NovoAluno = styled(Link)`
-  display: block;
-  padding: 20px 0 10px 0;
+export const Title = styled.h1`
+  text-align: center;
 `;
